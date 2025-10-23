370f09314dd48a136a867dcec408d160c4d90649
arancksj22 <arancksj@gmail.com>
2025-10-24 01:55:07 +0530

    spring boilerplate

---
+++ src/main/java/com/example/DockerSDKPractice/DockerSdkPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.DockerSDKPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSdkPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSdkPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DockerSDKPractice/DockerSdkPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerSDKPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSdkPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
