97b1497e67e325ff4087e9d8b99855c00bee545e
arancksj22 <arancksj@gmail.com>
2025-10-24 18:07:16 +0530

    testing with jersey server instead

--- src/main/java/DockerConfig.java
+++ src/main/java/DockerConfig.java
@@ -4,7 +4,8 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.netty.NettyDockerCmdExecFactory;
+import com.github.dockerjava.jaxrs.JerseyDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -17,17 +18,22 @@ public class DockerConfig {
 
     @Bean
     public DockerClient dockerClient() {
-        // Create config - will auto-detect Windows named pipe
+        // Auto-detect Docker configuration (will find the named pipe)
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
                 .build();
 
         log.info(">>>> Docker Host configured as: {} <<<<", config.getDockerHost());
 
-        // Use Netty factory explicitly (better for Windows named pipes)
-        DockerClient dockerClient = DockerClientImpl.getInstance(
-                config,
-                new NettyDockerCmdExecFactory()
-        );
+        // Use Jersey transport which supports Windows named pipes
+        // Timeouts are in milliseconds (Integer)
+        DockerHttpClient httpClient = new JerseyDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .connectTimeout(30000)  // 30 seconds in milliseconds
+                .readTimeout(45000)     // 45 seconds in milliseconds
+                .build();
+
+        DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
 
         log.info(">>>> DockerClient created successfully <<<<");
 
